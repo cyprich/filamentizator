305146e914acfb1ea3428a86bd3d04b3a57f377f
cyprich <cypooriginal@gmail.com>
2026-06-26 17:38:55 +0200

    frontend - empty project with tailwindcss and shadcn ui

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <p>Hello backend</p>
+      <Button>click</Button>
+    </>
+  );
+}
+
+export default App;
