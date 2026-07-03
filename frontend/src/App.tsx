a96bd1b38977b9186363a7c65b10851ab58250e8
cyprich <cypooriginal@gmail.com>
2026-07-03 13:49:05 +0200

    empty project with libraries installed

---
+++ frontend/src/App.tsx
@@ -0,0 +1,10 @@
+import {Button} from "@heroui/react";
+
+export default function App() {
+    return (
+        <div>
+            <p className={"text-red-500 font-bold"}>hello</p>
+            <Button>click</Button>
+        </div>
+    )
+}
